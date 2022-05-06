6e300676d197431fbed2ec2a24df1f51ad023a6a
c1t1gz <joaquin15poggi@gmail.com>
2022-05-06 19:33:38 -0300

    Calculo de Determinante 2x2

---
+++ codificacionDeHill/codificacion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+#include <unistd.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+using namespace std;
+
+
+int main() {
+
+}
+
+
